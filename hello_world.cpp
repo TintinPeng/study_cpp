367ba5c1f19a20913b3c6f33acab2a75aad6acc5
TintinPeng <2647206994@qq.com>
2020-07-30 18:50:13 +0800

    2020-7-30-6-50

---
+++ hello_world.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: tintin
+ * @Date: 2020-07-29 15:36:41
+ * @LastEditTime: 2020-07-29 15:38:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /study_cpp/hello_world.cpp
+ */
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+}
