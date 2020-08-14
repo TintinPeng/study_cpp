66578e4a14d3ad351bf3dcdf9d1bd1852bd22f60
TintinPeng <2647206994@qq.com>
2020-08-14 19:39:25 +0800

    c++

---
+++ src/practice/pr_3.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-14 19:06:03
+ * @LastEditTime: 2020-08-14 19:08:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /study_cpp/src/practice/pr_3.cpp
+ */
+
+#include "iostream"
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+    /* 
+     * 
+     */
+    int a = 350;
+    float b = 1.4;
+    double c = 1.4;
+
+    cout << a * b << "\t" << a * c << endl;
+    cout << b * a << "\t" << c * a << endl;
+    return 0;
+}
