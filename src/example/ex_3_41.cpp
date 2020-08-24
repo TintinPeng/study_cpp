306f53c88d9e587c3a54d08541b0db178f5d2df1
TintinPeng <2647206994@qq.com>
2020-08-24 20:47:05 +0800

    default

--- src/example/ex_3_41.cpp
+++ src/example/ex_3_41.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-24 19:16:34
- * @LastEditTime: 2020-08-24 19:25:03
+ * @LastEditTime: 2020-08-24 20:45:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /study_cpp/src/example/ex_3_41.cpp
@@ -42,5 +42,6 @@ int main(int argc, char const* argv[])
 {
     Date date1(1998, 4, 28);
     date1.showDate();
+    cout << "" << endl;
     return 0;
 }
