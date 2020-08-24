580649e59ae55e208664178e1e5702d6bedb96d7
TintinPeng <2647206994@qq.com>
2020-08-24 19:25:10 +0800

    default

---
+++ src/example/ex_3_41.cpp
@@ -0,0 +1,46 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-24 19:16:34
+ * @LastEditTime: 2020-08-24 19:25:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /study_cpp/src/example/ex_3_41.cpp
+ */
+#include "iostream"
+
+using namespace std;
+
+class Date {
+private:
+    const int year;
+    const int month;
+    const int day;
+
+public:
+    Date(int, int, int);
+    ~Date();
+    void showDate();
+};
+
+Date::Date(int y, int m, int d)
+    : year(y)
+    , month(m)
+    , day(d)
+{
+}
+
+Date::~Date()
+{
+}
+
+void Date::showDate()
+{
+    cout << year << "-" << month << "-" << day << endl;
+}
+
+int main(int argc, char const* argv[])
+{
+    Date date1(1998, 4, 28);
+    date1.showDate();
+    return 0;
+}
