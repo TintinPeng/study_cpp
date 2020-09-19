0d85f3ab4984bf28253b77384e28c86b7afcf017
TintinPeng <2647206994@qq.com>
2020-09-19 21:08:04 +0800

    default

--- src/example/ex_4_20.cpp
+++ src/example/ex_4_20.cpp
@@ -11,122 +11,122 @@
 using namespace std;
 
 class Building {
-private:
-        /* data */
+	private:
+	/* data */
 
-protected:
-        int    floors;
-        int    rooms;
-        double j_area;
+	protected:
+	int	   floors;
+	int	   rooms;
+	double j_area;
 
-public:
-        Building(int _floors = 0, int _rooms = 0, double _j_area = 0)
-        {
-                floors = _floors;
-                rooms = _rooms;
-                j_area = _j_area;
-        }
-        ~Building() { }
-        void show_Info()
-        {
-                cout << "楼房层数:" << floors << endl;
-                cout << "房间数:" << rooms << endl;
-                cout << "楼房总面积:" << j_area << endl;
-                cout << "其中:" << endl;
-        }
+	public:
+	Building(int _floors = 0, int _rooms = 0, double _j_area = 0)
+	{
+		floors = _floors;
+		rooms = _rooms;
+		j_area = _j_area;
+	}
+	~Building() { }
+	void show_Info()
+	{
+		cout << "楼房层数:" << floors << endl;
+		cout << "房间数:" << rooms << endl;
+		cout << "楼房总面积:" << j_area << endl;
+		cout << "其中:" << endl;
+	}
 };
 
 class Home_Arch : public Building {
-private:
-        int bedrooms;
-        int parlor;
-        int toilets;
-        int kitchens;
+	private:
+	int bedrooms;
+	int parlor;
+	int toilets;
+	int kitchens;
 
-protected:
-        /* data */
+	protected:
+	/* data */
 
-public:
-        Home_Arch(int _floors = 0, int _rooms = 0, double _j_area = 0,
-                int _bedrooms = 1, int _parlor = 1, int _toilets = 1, int _kitchens = 1)
-                : Building(_floors, _rooms, _j_area)
-        {
-                bedrooms = _bedrooms;
-                parlor = _parlor;
-                toilets = _toilets;
-                kitchens = _kitchens;
-        }
-        ~Home_Arch() { }
-        void show()
-        {
-                cout << endl;
-                cout << "住宅楼:" << endl;
-                Building::show_Info();
-                cout << "\t卧室数:" << bedrooms << endl;
-                cout << "\t客厅数:" << parlor << endl;
-                cout << "\t卫生间数:" << toilets << endl;
-                cout << "\t厨房数:" << kitchens << endl;
-        }
+	public:
+	Home_Arch(int _floors = 0, int _rooms = 0, double _j_area = 0,
+		int _bedrooms = 1, int _parlor = 1, int _toilets = 1, int _kitchens = 1)
+		: Building(_floors, _rooms, _j_area)
+	{
+		bedrooms = _bedrooms;
+		parlor = _parlor;
+		toilets = _toilets;
+		kitchens = _kitchens;
+	}
+	~Home_Arch() { }
+	void show()
+	{
+		cout << endl;
+		cout << "住宅楼:" << endl;
+		Building::show_Info();
+		cout << "\t卧室数:" << bedrooms << endl;
+		cout << "\t客厅数:" << parlor << endl;
+		cout << "\t卫生间数:" << toilets << endl;
+		cout << "\t厨房数:" << kitchens << endl;
+	}
 };
 
 class Office_Building : public Building {
-private:
-        int office;
-        int assembly_rooms;
+	private:
+	int office;
+	int assembly_rooms;
 
-protected:
-        /* data */
+	protected:
+	/* data */
 
-public:
-        Office_Building(int _floors = 0, int _rooms = 0, double _j_area = 0,
-                int _office = 0, int _assembly_rooms = 0)
-                : Building(_floors, _rooms, _j_area)
-        {
-                office = _office;
-                assembly_rooms = _assembly_rooms;
-        }
-        ~Office_Building() { }
-        void show()
-        {
-                cout << endl;
-                cout << "办公楼:" << endl;
-                Building::show_Info();
-                cout << "\t办公室数:" << office << endl;
-                cout << "\t会议室数:" << assembly_rooms << endl;
-        }
+	public:
+	Office_Building(int _floors = 0, int _rooms = 0, double _j_area = 0,
+		int _office = 0, int _assembly_rooms = 0)
+		: Building(_floors, _rooms, _j_area)
+	{
+		office = _office;
+		assembly_rooms = _assembly_rooms;
+	}
+	~Office_Building() { }
+	void show()
+	{
+		cout << endl;
+		cout << "办公楼:" << endl;
+		Building::show_Info();
+		cout << "\t办公室数:" << office << endl;
+		cout << "\t会议室数:" << assembly_rooms << endl;
+	}
 };
 
 class Hospital : public Building {
-private:
-        int sickrooms;
-        int operation_rooms;
+	private:
+	int sickrooms;
+	int operation_rooms;
 
-public:
-        Hospital(int _floors = 0, int _rooms = 0, double _j_area = 0,
-                int _sickrooms = 0, int _operation_rooms = 0)
-                : Building(_floors, _rooms, _j_area)
-        {
-                sickrooms = _sickrooms;
-                operation_rooms = _operation_rooms;
-        }
-        ~Hospital() { }
-        void show()
-        {
-                cout << endl;
-                cout << "医院:" << endl;
-                Building::show_Info();
-                cout << "\t病房数:" << sickrooms << endl;
-                cout << "\t手术室数:" << operation_rooms << endl;
-        }
+	public:
+	Hospital(int _floors = 0, int _rooms = 0, double _j_area = 0,
+		int _sickrooms = 0, int _operation_rooms = 0)
+		: Building(_floors, _rooms, _j_area)
+	{
+		sickrooms = _sickrooms;
+		operation_rooms = _operation_rooms;
+	}
+	~Hospital() { }
+	void show()
+	{
+		cout << endl;
+		cout << "医院:" << endl;
+		Building::show_Info();
+		cout << "\t病房数:" << sickrooms << endl;
+		cout << "\t手术室数:" << operation_rooms << endl;
+	}
 };
 
 int main(int argc, char const* argv[])
 {
-        Home_Arch       home(1, 100, 12000, 3, 1, 1, 1);
-        Office_Building office(4, 80, 3500, 40, 12);
-        Hospital        hospital(10, 300, 25000, 200, 20);
-        home.show();
-        office.show();
-        hospital.show();
-        return 0;
+	Home_Arch		home(1, 100, 12000, 3, 1, 1, 1);
+	Office_Building office(4, 80, 3500, 40, 12);
+	Hospital		hospital(10, 300, 25000, 200, 20);
+	home.show();
+	office.show();
+	hospital.show();
+	return 0;
 }
--- src/example/ex_4_21.cpp
+++ src/example/ex_4_21.cpp
@@ -12,129 +12,129 @@
 using namespace std;
 
 class Data_rec {
-protected:
-        string name;
-        char   sex;
-        int    age;
+	protected:
+	string name;
+	char   sex;
+	int	   age;
 
-public:
-        Data_rec(string _name, char _sex, int _age)
-        {
-                name = _name;
-                sex = _sex;
-                age = _age;
-        }
-        ~Data_rec() { }
-        void print()
-        {
-                cout << "name:" << name << endl;
-                cout << "sex:" << sex << endl;
-                cout << "age:" << age << endl;
-        }
+	public:
+	Data_rec(string _name, char _sex, int _age)
+	{
+		name = _name;
+		sex = _sex;
+		age = _age;
+	}
+	~Data_rec() { }
+	void print()
+	{
+		cout << "name:" << name << endl;
+		cout << "sex:" << sex << endl;
+		cout << "age:" << age << endl;
+	}
 };
 
 class Student : virtual public Data_rec {
-protected:
-        string major;
-        double score;
+	protected:
+	string major;
+	double score;
 
-public:
-        Student(string _name, char _sex, int _age,
-                string _major, double _score)
-                : Data_rec(_name, _sex, _age)
-        {
-                major = _major;
-                score = _score;
-        }
-        ~Student() { }
-        void print()
-        {
-                Data_rec::print();
-                cout << "score:" << score << endl;
-                cout << "major:" << major << endl;
-        }
+	public:
+	Student(string _name, char _sex, int _age,
+		string _major, double _score)
+		: Data_rec(_name, _sex, _age)
+	{
+		major = _major;
+		score = _score;
+	}
+	~Student() { }
+	void print()
+	{
+		Data_rec::print();
+		cout << "score:" << score << endl;
+		cout << "major:" << major << endl;
+	}
 };
 
 class Employee : virtual public Data_rec {
-protected:
-        string dept;
-        double salary;
+	protected:
+	string dept;
+	double salary;
 
-public:
-        Employee(string _name, char _sex, int _age, string _dept, double _salary)
-                : Data_rec(_name, _sex, _age)
-        {
-                dept = _dept;
-                salary = _salary;
-        }
-        ~Employee() { }
-        void print()
-        {
-                Data_rec::print();
-                cout << "department:" << dept << endl;
-                cout << "salary:" << salary << endl;
-        }
+	public:
+	Employee(string _name, char _sex, int _age, string _dept, double _salary)
+		: Data_rec(_name, _sex, _age)
+	{
+		dept = _dept;
+		salary = _salary;
+	}
+	~Employee() { }
+	void print()
+	{
+		Data_rec::print();
+		cout << "department:" << dept << endl;
+		cout << "salary:" << salary << endl;
+	}
 };
 
 class Teacher : public Employee {
-private:
-        /* data */
+	private:
+	/* data */
 
-protected:
-        string title;
+	protected:
+	string title;
 
-public:
-        Teacher(string _name, char _sex, int _age,
-                string _dept, double _salary, string _title)
-                : Data_rec(_name, _sex, _age)
-                , Employee(_name, _sex, _age, _dept, _salary)
-        {
-                title = _title;
-        }
-        ~Teacher() { }
-        void print()
-        {
-                Employee::print();
-                cout << "title:" << title << endl;
-        }
+	public:
+	Teacher(string _name, char _sex, int _age,
+		string _dept, double _salary, string _title)
+		: Data_rec(_name, _sex, _age)
+		, Employee(_name, _sex, _age, _dept, _salary)
+	{
+		title = _title;
+	}
+	~Teacher() { }
+	void print()
+	{
+		Employee::print();
+		cout << "title:" << title << endl;
+	}
 };
 
 class E_Student : public Employee, public Student {
-public:
-        E_Student(string _name, char _sex, int _age,
-                string _major, double _score,
-                string _dept, double _salary)
-                : Data_rec(_name, _sex, _age)
-                , Student(_name, _sex, _age, _major, _score)
-                , Employee(_name, _sex, _age, _dept, _salary)
-        {
-        }
-        ~E_Student() { }
-        void print()
-        {
-                Student::print();
-                cout << "department:" << dept << endl;
-                cout << "salary:" << salary << endl;
-        }
+	public:
+	E_Student(string _name, char _sex, int _age,
+		string _major, double _score,
+		string _dept, double _salary)
+		: Data_rec(_name, _sex, _age)
+		, Student(_name, _sex, _age, _major, _score)
+		, Employee(_name, _sex, _age, _dept, _salary)
+	{
+	}
+	~E_Student() { }
+	void print()
+	{
+		Student::print();
+		cout << "department:" << dept << endl;
+		cout << "salary:" << salary << endl;
+	}
 };
 
 int main(int argc, char const* argv[])
 {
-        Student stu("李晓敏", 'f', 22, "应用数学", 90);
-        cout << "Student:" << endl;
-        stu.print();
+	Student stu("李晓敏", 'f', 22, "应用数学", 90);
+	cout << "Student:" << endl;
+	stu.print();
 
-        Employee emp("黄柏松", 'm', 55, "科研处", 5000);
-        cout << "Employee:" << endl;
-        emp.print();
+	Employee emp("黄柏松", 'm', 55, "科研处", 5000);
+	cout << "Employee:" << endl;
+	emp.print();
 
-        Teacher tea("王世明", 'm', 34, "管理学院", 8000, "教授");
-        cout << "Teacher:" << endl;
-        tea.print();
+	Teacher tea("王世明", 'm', 34, "管理学院", 8000, "教授");
+	cout << "Teacher:" << endl;
+	tea.print();
 
-        E_Student E_Stu("张大", 'f', 34, "计算机", 95, "教务处", 3500);
-        cout << "E_Student:" << endl;
-        E_Stu.print();
+	E_Student E_Stu("张大", 'f', 34, "计算机", 95, "教务处", 3500);
+	cout << "E_Student:" << endl;
+	E_Stu.print();
 
-        return 0;
+	return 0;
 }
