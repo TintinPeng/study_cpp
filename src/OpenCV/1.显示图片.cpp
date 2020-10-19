05cdd39e9d7a2f39fe3179d5799a8707e3384113
TintinPeng <2647206994@qq.com>
2020-10-19 20:40:50 +0800

    default

---
+++ src/OpenCV/1.显示图片.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include "opencv4/opencv2/core.hpp"
+#include "opencv4/opencv2/highgui.hpp"
+#include "opencv4/opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+const string workspace = "/home/tintin/Workspaces/VSCode/cpp/src/OpenCV";
+
+int main()
+{
+	string filename = workspace + "/pictures/001.png";
+	Mat	   img = imread(filename);
+	namedWindow(filename, WINDOW_AUTOSIZE);
+	imshow(filename, img);
+	waitKey(0);
+	return 0;
+}
