06cf408cab4349c5ef968f11656972f620eca83a
TintinPeng <2647206994@qq.com>
2020-11-23 13:32:12 +0800

    default

--- src/Example/2.面向对象/ex_5_2.cpp
+++ src/Example/2.面向对象/ex_5_2.cpp
@@ -1,79 +1,71 @@
-/*
- * @Author: your name
- * @Date: 2020-09-07 09:24:29
- * @LastEditTime: 2020-09-07 09:55:52
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /study_cpp/src/example/ex_5_2.cpp
- */
-#include "iostream"
+#include <iostream>
 
 using namespace std;
 
 class Complex {
 private:
-        double real;
-        double imag;
+	double real;
+	double imag;
 
 protected:
-        /* data */
+	/* data */
 
 public:
-        Complex(double, double);
-        ~Complex() { }
-        void           print();
-        friend Complex operator+(Complex&, Complex&);
-        friend Complex operator-(Complex&, Complex&);
-        friend Complex operator*(Complex&, Complex&);
-        friend Complex operator/(Complex&, Complex&);
+	Complex(double, double);
+	~Complex() { }
+	void		   print();
+	friend Complex operator+(Complex&, Complex&);
+	friend Complex operator-(Complex&, Complex&);
+	friend Complex operator*(Complex&, Complex&);
+	friend Complex operator/(Complex&, Complex&);
 };
 
 Complex::Complex(double r = 0.0, double i = 0.0)
 {
-        real = r;
-        imag = i;
+	real = r;
+	imag = i;
 }
 
 Complex operator+(Complex& a, Complex& b)
 {
-        return Complex(a.real + b.real, a.imag + b.imag);
+	return Complex(a.real + b.real, a.imag + b.imag);
 }
 
 Complex operator-(Complex& a, Complex& b)
 {
-        return Complex(a.real - b.real, a.imag - b.imag);
+	return Complex(a.real - b.real, a.imag - b.imag);
 }
 
 Complex operator*(Complex& a, Complex& b)
 {
-        return Complex(a.real * b.real - a.imag * b.imag, a.real * b.imag + a.imag * b.real);
+	return Complex(a.real * b.real - a.imag * b.imag, a.real * b.imag + a.imag * b.real);
 }
 
 Complex operator/(Complex& a, Complex& b)
 {
-        double t;
-        t = 1 / (b.real * b.real + b.imag * b.imag);
-        return Complex((a.real * b.real + a.imag * b.imag) * t, (b.real * a.imag - a.real * b.imag) * t);
+	double t;
+	t = 1 / (b.real * b.real + b.imag * b.imag);
+	return Complex((a.real * b.real + a.imag * b.imag) * t, (b.real * a.imag - a.real * b.imag) * t);
 }
 
 void Complex::print()
 {
-        cout << real;
-        if (imag > 0)
-                cout << "+";
-        if (imag != 0)
-                cout << imag << "i" << endl;
+	cout << real;
+	if (imag > 0)
+		cout << "+";
+	if (imag != 0)
+		cout << imag << "i" << endl;
 }
 
 int main(int argc, char const* argv[])
 {
-        Complex A[6] = { Complex(2.3, 4.6), Complex(3.6, 2.8) };
-        A[2] = A[0] + A[1];
-        A[3] = A[0] - A[1];
-        A[4] = A[0] * A[1];
-        A[5] = A[0] / A[1];
-        for (int i = 0; i < 6; i++) {
-                A[i].print();
-        }
-        return 0;
+	Complex A[6] = { Complex(2.3, 4.6), Complex(3.6, 2.8) };
+	A[2] = A[0] + A[1];
+	A[3] = A[0] - A[1];
+	A[4] = A[0] * A[1];
+	A[5] = A[0] / A[1];
+	for (int i = 0; i < 6; i++) {
+		A[i].print();
+	}
+	return 0;
 }
--- src/Example/2.面向对象/ex_5_3.cpp
+++ src/Example/2.面向对象/ex_5_3.cpp
@@ -1,41 +1,33 @@
-/*
- * @Author: your name
- * @Date: 2020-09-07 10:09:18
- * @LastEditTime: 2020-09-07 11:15:50
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /study_cpp/src/example/ex_5_3.cpp
- */
-#include "iostream"
+#include <iostream>
 
 using namespace std;
 
 class Coord {
 private:
-        int x, y;
+	int x, y;
 
 protected:
-        /* data */
+	/* data */
 
 public:
-        Coord(int _x = 0, int _y = 0) { x = _x, y = _y; }
-        ~Coord() { }
-        friend Coord operator-(Coord& obj)
-        {
-                return Coord(-obj.x, -obj.y);
-        }
-        void print()
-        {
-                cout << "x=" << x << "\t"
-                     << "y=" << y << endl;
-        }
+	Coord(int _x = 0, int _y = 0) { x = _x, y = _y; }
+	~Coord() { }
+	friend Coord operator-(Coord& obj)
+	{
+		return Coord(-obj.x, -obj.y);
+	}
+	void print()
+	{
+		cout << "x=" << x << "\t"
+			 << "y=" << y << endl;
+	}
 };
 
 int main(int argc, char const* argv[])
 {
-        Coord ob1(20, 30), ob2;
-        ob2 = -ob1;
-        ob1.print();
-        ob2.print();
-        return 0;
+	Coord ob1(20, 30), ob2;
+	ob2 = -ob1;
+	ob1.print();
+	ob2.print();
+	return 0;
 }
